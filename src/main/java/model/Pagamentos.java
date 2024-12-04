277933ca372f2ae3b7dddcdc42e6883e8a26a82a
F6065717 Karine Alves De Freitas Leite <karineleite@bb.com.br>
2024-12-04 15:12:58 -0300

    Metricas personalizadas adicionadas

--- src/main/java/model/Pagamentos.java
+++ src/main/java/model/Pagamentos.java
@@ -2,7 +2,6 @@ package model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 
 @Entity
--- src/main/java/rest/PagamentoResource.java
+++ src/main/java/rest/PagamentoResource.java
@@ -1,5 +1,9 @@
 package rest;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -10,12 +14,32 @@ import java.util.List;
 import org.jboss.logging.Logger;
 
 @Path("/pagamentos")
-public class PagamentoSqlResource {
+public class PagamentoResource {
 
-    private static final Logger log = Logger.getLogger(PagamentoSqlResource.class);
+    private Counter erroInsercao;
+    private Counter sucessoInsercao;
+
+    @Inject
+    MeterRegistry registry;
+
+    private static final Logger log = Logger.getLogger(PagamentoResource.class);
 
     @Inject
     PagamentoService pagamentoService;
+
+    void inicializaMetricas(@Observes StartupEvent ev) {
+        this.erroInsercao = Counter.builder("erros_insercao_usuario")
+                              .description("Erros no json dos usuários")
+                              .register(registry);
+
+        this.sucessoInsercao = Counter.builder("sucessos_insercao_usuario")
+                .description("Sucessos no json dos usuários")
+                .register(registry);
+
+        this.erroInsercao.increment(0);
+        this.sucessoInsercao.increment(0);
+
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -23,13 +47,15 @@ public class PagamentoSqlResource {
     public Response criarPagamento(Pagamentos pagamentos) throws Exception {
         try {
             pagamentoService.salvarPagamento(pagamentos);
+            sucessoInsercao.increment();
             return Response.status(Response.Status.CREATED)
                     .entity(pagamentos)
                     .build();
         }
         catch (Exception e) {
             log.error("Erro ao criar pagamento", e);
-            return Response.status(Response.Status.BAD_REQUEST)
+            erroInsercao.increment();
+            return Response.status(422)
                     .entity(e.getMessage())
                     .build();
         }
@@ -39,6 +65,7 @@ public class PagamentoSqlResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response obterPagamentos() {
         List<Pagamentos> pagamentos = pagamentoService.obterPagamentos();
+        erroInsercao.increment();
         return Response.ok(pagamentos).build();
     }
 
